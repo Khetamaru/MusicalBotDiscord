5e16a522fb248b8fdd9c32a89712fa4fcbf88c33
Benoit SCHULER <bschuler@avencall.com>
2021-10-24 18:57:12 +0200

    Add ping-pong bot with kord

--- yisan/src/main/kotlin/fr/school/yisan/YisanApplication.kt
+++ yisan/src/main/kotlin/fr/school/yisan/YisanApplication.kt
@@ -3,6 +3,9 @@ package fr.school.yisan
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import com.jagrosh.jdautilities.command.CommandClientBuilder
+import dev.kord.core.Kord
+import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.on
 import net.dv8tion.jda.api.AccountType
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
@@ -12,22 +15,23 @@ import net.dv8tion.jda.api.entities.Activity
 @SpringBootApplication
 class YisanApplication
 
-private lateinit var jda: JDA
-private const val TOKEN = "MY_TOKEN" //Jeton Bot obtenu
+private const val TOKEN = "token" //Jeton Bot obtenu
 private const val COMMAND_PREFIX = "!" //Préfixe de commande
 
-fun main(args: Array<String>) {
+suspend fun main() {
+	val kord = Kord(TOKEN)
 
-	runApplication<YisanApplication>(*args)
-	//Générer un écouteur d'événements qui gère les commandes
-	val commandClient = CommandClientBuilder()
-			.setPrefix(COMMAND_PREFIX) //Préfixe de commande
-			.setStatus(OnlineStatus.ONLINE) //Paramètres d'état en ligne
-			.setActivity(Activity.watching("YouTube")) //Paramètres d'état (regarder, jouer, etc.)
-			.build()
+	kord.on<MessageCreateEvent> {//runs every time a message is created that our bot can read
 
-	jda = JDABuilder(AccountType.BOT)
-			.setToken(TOKEN) //Définir le jeton
-			.addEventListeners(commandClient) //set commandClient
-			.build()
+		//ignore other bots, even ourselves. We only serve humans here!
+		if(message.author?.isBot != false) return@on
+
+		//check if our command is being invoked
+		if(message.content != "!ping") return@on
+
+		//all clear, give them the pong!
+		message.channel.createMessage("pong!")
+	}
+
+	kord.login()
 }
